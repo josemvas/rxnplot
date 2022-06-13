60c02d8c8041f4724b6ea576367f5e7d900fe0ad
cronofugo <manuelvsqz@gmail.com>
2022-06-13 15:09:36 -0500

    Remove setup files from repo

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-#min_stversion = '30.3' # Minimum Setuptools version supporting configuration metadata in setup.cfg
-min_stversion = '32.2' # Minimum Setuptools version supporting conditional python dependencies (PEP 508)
-
-# Setup package if Setuptools version is high enough
-setuptools.setup(setup_requires=['setuptools>=' + min_stversion])
-
