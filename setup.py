d2cf6e5791bc48790801e5e64c7f1fe0b9c99e47
cronofugo <manuelvsqz@gmail.com>
2022-06-17 03:34:03 -0500

    Bug fixes

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ from shutil import rmtree
 setup()
 
 if getuser() == 'jovyan' and argv[1] == 'bdist_wheel':
-    rmtree(path.join(here, 'build'))
-    rmtree(path.join(here, 'rxnplot.egg-info'))
-    remove(path.join(here, 'image1.png'))
-    remove(path.join(here, 'README.md'))
+    here = path.abspath(path.dirname(__file__))
     remove(path.join(here, 'LICENSE'))
+    remove(path.join(here, 'README.md'))
+    remove(path.join(here, 'image1.png'))
     remove(path.join(here, 'setup.cfg'))
     remove(path.join(here, 'setup.py'))
+    rmtree(path.join(here, 'build'))
+    rmtree(path.join(here, 'rxnplot.egg-info'))
