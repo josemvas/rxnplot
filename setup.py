52b6786c809ec6c35846c2df3699aab4fafe4ed2
cronofugo <manuelvsqz@gmail.com>
2022-06-16 23:51:36 -0500

    Clean after setup

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from sys import argv
 from setuptools import setup
 from shutil import rmtree
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'setup.log'), 'w') as f:
+    f.write(repr(argv))
+
 setup()
+
 if 'install' in argv:
-    rmtree('build')
-    rmtree('rxnplot.egg-info')
+    rmtree(path.join(here, 'build'))
+    rmtree(path.join(here, 'rxnplot.egg-info'))
