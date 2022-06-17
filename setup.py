9bcdf96cfd8f6b78f4923c77a86ec594f2f4eec1
cronofugo <manuelvsqz@gmail.com>
2022-06-17 01:27:54 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
+from sys import argv
 from setuptools import setup
-from socket import gethostname
-from os import path, environ, remove
+from os import path, remove, environ
 from shutil import rmtree
-import sys
-
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'setup.log'), 'w') as f:
-    f.write(repr(sys.argv))
 
 setup()
 
-#if gethostname().startswith('jupyter-'):
-#    rmtree(path.join(here, 'build'))
-#    rmtree(path.join(here, 'rxnplot.egg-info'))
-#    remove(path.join(here, 'README.md'))
-#    remove(path.join(here, 'image1.png'))
-#    remove(path.join(here, 'setup.cfg'))
-#    remove(path.join(here, 'setup.py'))
+if 'BINDER_LAUNCH_HOST' in environ:
+    here = path.abspath(path.dirname(__file__))
+    rmtree(path.join(here, 'build'))
+    rmtree(path.join(here, 'rxnplot.egg-info'))
+    remove(path.join(here, 'README.md'))
+    remove(path.join(here, 'image1.png'))
+    remove(path.join(here, 'setup.cfg'))
+    remove(path.join(here, 'setup.py'))
