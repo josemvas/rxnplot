cd1ce1a84ad5fbaaecb1eaed8ca010475bad4305
cronofugo <manuelvsqz@gmail.com>
2022-06-17 02:07:58 -0500

    More tests

--- setup.py
+++ setup.py
@@ -2,24 +2,21 @@ from sys import argv
 from setuptools import setup
 from os import path, remove, environ
 from shutil import rmtree
-from re import fullmatch
+from getpass import getuser
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'setup.log'), 'w') as f:
     f.write(repr(argv))
 with open(path.join(here, 'environ.log'), 'w') as f:
     for key, value in environ.items():
-        f.write('{}={}'.format(key, value))
+        f.write('{}={}\n'.format(key, value))
 
 setup()
 
-try:
-    if argv[2] == '-d' and fullmatch('/tmp/pip-wheel-[a-z0-9]{8}', argv[3]):
-        rmtree(path.join(here, 'build'))
-        rmtree(path.join(here, 'rxnplot.egg-info'))
-        remove(path.join(here, 'README.md'))
-        remove(path.join(here, 'image1.png'))
-        remove(path.join(here, 'setup.cfg'))
-        remove(path.join(here, 'setup.py'))
-except IndexError:
-    pass
+if getuser() == 'jovyan':
+    rmtree(path.join(here, 'build'))
+    rmtree(path.join(here, 'rxnplot.egg-info'))
+    remove(path.join(here, 'README.md'))
+    remove(path.join(here, 'image1.png'))
+    remove(path.join(here, 'setup.cfg'))
+    remove(path.join(here, 'setup.py'))
