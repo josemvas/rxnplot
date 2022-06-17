b12fb60032df993b71457c74ff52aaeedd77f2ff
cronofugo <manuelvsqz@gmail.com>
2022-06-17 01:53:36 -0500

    Tests

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 from sys import argv
 from setuptools import setup
-from os import path, remove
+from os import path, remove, environ
 from shutil import rmtree
 from re import fullmatch
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'setup.log'), 'w') as f:
+    f.write(repr(argv))
+    f.write(environ.get('BINDER_LAUNCH_HOST', 'UNSET'))
 
 setup()
 
 try:
     if argv[2] == '-d' and fullmatch('/tmp/pip-wheel-[a-z0-9]{8}', argv[3]):
-        here = path.abspath(path.dirname(__file__))
         rmtree(path.join(here, 'build'))
         rmtree(path.join(here, 'rxnplot.egg-info'))
         remove(path.join(here, 'README.md'))
         remove(path.join(here, 'image1.png'))
         remove(path.join(here, 'setup.cfg'))
         remove(path.join(here, 'setup.py'))
+        with open(path.join(here, 'remove.log'), 'w') as f:
+            f.write('sucess')
 except IndexError:
     pass
