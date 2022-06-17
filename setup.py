b94add9e97b64d6b3f6b6cdcfe546bbc4e05b645
cronofugo <manuelvsqz@gmail.com>
2022-06-17 18:02:55 -0500

    Add postBuild script

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from shutil import rmtree
 
 setup()
 
-if getuser() == 'jovyan' and argv[1] == 'bdist_wheel':
-    here = path.abspath(path.dirname(__file__))
-    remove(path.join(here, 'LICENSE'))
-    remove(path.join(here, 'README.md'))
-    remove(path.join(here, 'image1.png'))
-    remove(path.join(here, 'setup.cfg'))
-    remove(path.join(here, 'setup.py'))
-    rmtree(path.join(here, 'build'))
-    rmtree(path.join(here, 'rxnplot'))
-    rmtree(path.join(here, 'rxnplot.egg-info'))
+#if getuser() == 'jovyan' and argv[1] == 'bdist_wheel':
+#    here = path.abspath(path.dirname(__file__))
+#    remove(path.join(here, 'LICENSE'))
+#    remove(path.join(here, 'README.md'))
+#    remove(path.join(here, 'image1.png'))
+#    remove(path.join(here, 'setup.cfg'))
+#    remove(path.join(here, 'setup.py'))
+#    rmtree(path.join(here, 'build'))
+#    rmtree(path.join(here, 'rxnplot'))
+#    rmtree(path.join(here, 'rxnplot.egg-info'))
