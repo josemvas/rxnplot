a4aa5b15b570112d7abb17b6a4f9c1b145f19f11
cronofugo <manuelvsqz@gmail.com>
2022-06-17 00:20:14 -0500

    Bug fixes

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from shutil import rmtree
 
 setup()
 
-if 'BINDER_SERVICE_HOST' in os.environ:
+if 'BINDER_SERVICE_HOST' in environ:
     here = path.abspath(path.dirname(__file__))
     rmtree(path.join(here, 'build'))
     rmtree(path.join(here, 'rxnplot.egg-info'))
-    remove(path.join(here, 'image1.png')
-    remove(path.join(here, 'setup.cfg')
-    remove(path.join(here, 'setup.py')
+    remove(path.join(here, 'image1.png'))
+    remove(path.join(here, 'setup.cfg'))
+    remove(path.join(here, 'setup.py'))
