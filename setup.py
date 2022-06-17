e2031169981f13899ab67eadcce31dcf4f188984
cronofugo <manuelvsqz@gmail.com>
2022-06-17 03:29:04 -0500

    Bug fixes

--- setup.py
+++ setup.py
@@ -1,27 +1,16 @@
 from sys import argv
-from setuptools import setup
-from os import path, remove, environ, listdir, getpid
-from shutil import rmtree
 from getpass import getuser
-
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, str(getpid()) + '_setup.log'), 'w') as f:
-    f.write(repr(argv))
-with open(path.join(here, str(getpid()) + '_environ.log'), 'w') as f:
-    for key, value in environ.items():
-        f.write('{}={}\n'.format(key, value))
+from setuptools import setup
+from os import path, remove
+from shutil import rmtree
 
 setup()
 
-if getuser() == 'jovyan':
-#    if path.isdir(path.join(here, 'build')):
-#        rmtree(path.join(here, 'build'))
-#    if path.isdir(path.join(here, 'rxnplot.egg-info')):
-#        rmtree(path.join(here, 'rxnplot.egg-info'))
-#    remove(path.join(here, 'README.md'))
-#    remove(path.join(here, 'image1.png'))
-#    remove(path.join(here, 'setup.cfg'))
-#    remove(path.join(here, 'setup.py'))
-    with open(path.join(here, str(getpid()) + '_listdir.log'), 'w') as f:
-        for item in listdir():
-            f.write('{}\n'.format(item))
+if getuser() == 'jovyan' and arvg[1] == 'bdist_wheel':
+    rmtree(path.join(here, 'build'))
+    rmtree(path.join(here, 'rxnplot.egg-info'))
+    remove(path.join(here, 'image1.png'))
+    remove(path.join(here, 'README.md'))
+    remove(path.join(here, 'LICENSE'))
+    remove(path.join(here, 'setup.cfg'))
+    remove(path.join(here, 'setup.py'))
